636bccf95e8214245d77a64249da4268ffb683b0
doorkiPr <boumarafchakib7@gmail.com>
2023-12-13 00:00:14 +0100

    ADD:encapsulated logic inside function

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const grid = document.querySelector('.grid');
 const slider = document.querySelector('input');
 
-let squareNumbers = prompt('how many squares do u want ');
-squareNumbers = squareNumbers*squareNumbers;
+const generateGrid = (squareNumbers) => {
+    for (let i = 0; i < squareNumbers; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.height = `${500 * 1 / Math.sqrt(squareNumbers) - 2}px`;
+        square.style.width = `${500 * 1 / Math.sqrt(squareNumbers) - 2}px`;
+        // math equation we divide the grid height by the square root of the number of squares , and substract 2px to offset the border
+        grid.appendChild(square);
+    }
 
-for (let i = 0; i < squareNumbers; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.height=`${500*1/Math.sqrt(squareNumbers) - 2}px`;
-    square.style.width=`${500*1/Math.sqrt(squareNumbers) - 2}px`;
-    // math equation we divide the grid height by the square root of the number of squares , and substract 2px to offset the border
-    grid.appendChild(square);
 }
