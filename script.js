66cc30ddcdfcba110ae9be67ca532497d38e3371
doorkiPr <boumarafchakib7@gmail.com>
2023-12-12 22:08:54 +0100

    ADD:add logic to calculate size of each square

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const grid = document.querySelector('.grid');
-const square = document.createElement('div');
-square.classList.add('grid');
-square.style.border = '1px black solid';
-square.style.width = '25px';
-square.style.height = '25px';
+
+
+for (let i = 0; i < 16; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.height=`${500*1/Math.sqrt(16) - 2}px`;
+    square.style.width=`${500*1/Math.sqrt(16) - 2}px`;
+    // math equation we divide the grid height by the square root of the number of squares , and substract 2px to offset the border
+    grid.appendChild(square);
+    
+}
