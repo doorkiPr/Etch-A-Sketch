2d5e51708a5a6998862bdf02705b9d02b126ea08
doorkiPr <boumarafchakib7@gmail.com>
2023-12-12 09:12:19 +0100

    ADD:add square div give it a class  and style it

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const grid = document.querySelector('.grid');
+const square = document.createElement('div');
+square.classList.add('grid');
+square.style.border = '1px black solid';
+square.style.width = '25px';
+square.style.height = '25px';
