8aa6ef7162304e7d5ebd6e9fee247c6c6189dcd6
doorkiPr <boumarafchakib7@gmail.com>
2023-12-12 22:45:32 +0100

    FIX:fix typos

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const grid = document.querySelector('.grid');
 
-let squareNumbes = prompt('how many squares do u want ');
-squareNumbes = squareNumbes*squareNumbes;
+let squareNumbers = prompt('how many squares do u want ');
+squareNumbers = squareNumbers*squareNumbers;
 
-for (let i = 0; i < squareNumbes; i++) {
+for (let i = 0; i < squareNumbers; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.style.height=`${500*1/Math.sqrt(squareNumbes) - 2}px`;
-    square.style.width=`${500*1/Math.sqrt(squareNumbes) - 2}px`;
+    square.style.height=`${500*1/Math.sqrt(squareNumbers) - 2}px`;
+    square.style.width=`${500*1/Math.sqrt(squareNumbers) - 2}px`;
     // math equation we divide the grid height by the square root of the number of squares , and substract 2px to offset the border
     grid.appendChild(square);
 }
